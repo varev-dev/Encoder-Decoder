15de2c068c0f342488e93020e16cf87f4915d3bb
varev-dev <KACDOGA4@edu.gdansk.pl>
2020-08-24 01:15:18 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,58 +1,121 @@
 package encryptdecrypt;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
+    static String mode = "enc";
+    static String outputFile = "";
+    static int key = 0;
+
     public static void main(String[] args) {
-        String mode = "enc";
-        int key = 0;
         String data = "";
-
         for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-mode")) {
-                mode = args[i + 1];
-            } else if (args[i].equals("-key")) {
-                key = Integer.parseInt(args[i + 1]);
-            } else if (args[i].equals("-data")) {
-                data = args[i + 1];
+            switch (args[i]) {
+                case "-mode":
+                    mode = args[++i];
+                    break;
+                case "-data":
+                    data = args[++i];
+                    break;
+                case "-in":
+                    String inputFile = args[++i];
+                    for (String arg : args) {
+                        if (arg.equals("-data")) {
+                            break;
+                        } else {
+                            try {
+                                data = new String(Files.readAllBytes(Paths.get(inputFile)));
+                            } catch (IOException e) {
+                                System.out.println("Error1");
+                            }
+                        }
+                    }
+                    break;
+                case "-out":
+                    outputFile = args[++i];
+                    break;
+                case "-key":
+                    try {
+                        key = Integer.parseInt(args[++i]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Error2");
+                    }
+                    break;
             }
         }
 
+        String result = "";
         switch (mode) {
-            case "dec":
-                simpleDecrypt(data, key);
-                break;
             case "enc":
-                simpleEncrypt(data, key);
+                encrypt(data, key, result);
                 break;
+            case "dec":
+                decrypt(data, key, result);
+                break;
+            default:
+                System.out.println("Error3");
         }
     }
 
-    public static void simpleEncrypt(String toEncrypt, int encryptDiff) {
-        char[] toEncryptArray;
-        toEncryptArray = toEncrypt.toCharArray();
+    public static void encrypt(String data, int key, String result) {
+        if (data.toCharArray().length != 0) {
+            char[] toEncryptArray;
+            toEncryptArray = data.toCharArray();
 
-        for (int i = 0; i < toEncryptArray.length; i++) {
-            for (int j = 1; j <= encryptDiff; j++) {
-                toEncryptArray[i] += 1;
-                if (toEncryptArray[i] > 126) {
-                    toEncryptArray[i] = 32;
+            StringBuilder resultBuilder = new StringBuilder(result);
+            for (int i = 0; i < toEncryptArray.length; i++) {
+                for (int j = 1; j <= key; j++) {
+                    toEncryptArray[i] += 1;
+                    if (toEncryptArray[i] > 126) {
+                        toEncryptArray[i] = 32;
+                    }
                 }
+                resultBuilder.append(toEncryptArray[i]);
             }
-            System.out.printf("%c", toEncryptArray[i]);
+            result = resultBuilder.toString();
+
+            overwriteFiles(result, outputFile);
         }
     }
 
-    public static void simpleDecrypt(String toDecrypt, int decryptDiff) {
+    public static void decrypt(String data, int key, String result) {
         char[] toDecryptArray;
-        toDecryptArray = toDecrypt.toCharArray();
+        toDecryptArray = data.toCharArray();
 
+        StringBuilder resultBuilder = new StringBuilder(result);
         for (int i = 0; i < toDecryptArray.length; i++) {
-            for (int j = 1; j <= decryptDiff; j++) {
+            for (int j = 1; j <= key; j++) {
                 toDecryptArray[i] -= 1;
                 if (toDecryptArray[i] < 32) {
                     toDecryptArray[i] = 126;
                 }
             }
-            System.out.printf("%c", toDecryptArray[i]);
+            resultBuilder.append(toDecryptArray[i]);
+        }
+        result = resultBuilder.toString();
+
+        overwriteFiles(result, outputFile);
+    }
+
+
+    public static void overwriteFiles(String result, String outputFile) {
+        if (outputFile.equals("")) {
+            System.out.println(result);
+        } else {
+            try {
+                File file = new File(outputFile);
+                if (!file.createNewFile()) {
+                    System.out.println("Error4");
+                }
+                FileWriter writer = new FileWriter(file);
+                writer.write(result);
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("Error5");
+            }
+
         }
     }
 }
